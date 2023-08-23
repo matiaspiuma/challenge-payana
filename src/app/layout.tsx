5bfafd4f7324bbf118e3f61f2b59eec6dc96bfd0
Matias Piuma <matius@gmail.com>
2023-08-23 18:10:10 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <main className="max-w-screen-lg m-auto p-4">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
